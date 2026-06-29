b6bf630687ee9f056fe977c45ce966904cca0928
BilalsGituation <billyasser@hotmail.co.uk>
2026-06-29 23:13:19 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,89 @@
+Date released: 2026-06-29
+
+Author: Bilal Asser
+
+(Hardware needs minimum 16GB memory to run this)
+
+## Purpose of this work:
+This workflow provides a practical intro to Tumour Microenvironment (TME) analysis using Spatial Transcriptomics data (10X Visium), for learners who have some experience with single cell and spatial transcriptomics analysis in Seurat through the vignettes.
+
+## Quick note on breast cancer TME:
+DCIS is Ductal Carcinoma in situ. These cells are preinvasive cancer cells still confined to the milk ducts
+
+## Quick concept tutorial for later:
+- Normalised Mutual Information (NMI) is a measure of the similarity of two images.
+- NiftyReg is a visual pattern detection algorithm which finds and registers statistical transformations on a source image to make it more similar to a query image (increasing their NMI score).
+(statistical transformation examples: rotation, scale, shear, translation)
+    - If two images lack shared structural patterns, NiftyReg cannot artificially increase their similarity.
+
+## Aims: 
+1) Spatial transcriptomics workflow interoperation by translating the OSTA chapter 12 workflow to the Seurat framework
+2) Visually compare differences in spatial maps produced by our deconvolution using `Seurat::FindTransferAnchors()` and OSTA's deconvolution using RCTD
+3) Use NMI to quantify the change in spatial alignment between spatial maps from `Seurat::FindTransferAnchors()` and RCTD after affine registration with the NiftyReg algorithm
+4) Calculate match rates between Seurat predictions and ground truth supplied by 10X
+
+(Bonus) Visually compare Seurat's UMAP clustering vs the ground truth cell type IDs provided by 10X genomics.
+
+## Questions: 
+- Do the spatial maps from this TME dataset visually differ when produced with Seurat’s integration-based deconvolution instead of RCTD’s probabilistic deconvolution? 
+- What is the increase in NMI achieved by optimally aligning the Seurat map to the RCTD map using affine transformations? 
+- How close are Seurat's cell type predictions to the annotation provided by 10X?
+
+## Links to project inspiration:
+
+Use this link to see the original workflow the OSTA ebook authors wrote, that I adapted this script from
+
+https://bioconductor.org/books/release/OSTA/pages/seq-deconvolution.html
+
+To skip to the images
+(Note: Deconvolution result images are flipped vertically in this link)
+
+https://bioconductor.org/books/release/OSTA/pages/seq-deconvolution.html#visualization
+
+## Limitations:
+- This version only calculates a confusion matrix on the Seurat results.
+- The sample is one spatial transcriptomics slide/slice.
+- No analysis is made into why one prediction algorithm captures certain oncological info better than the other.
+- Did not find markers for clusters.
+
+## Conclusion ####
+
+In a sentence:
+The breast cancer cells were difficult to identify, even using reference-based deconvolution.
+
+### What evidence went into this?
+#### 1) NMI Similarity (Seurat vs RCTD)
+
+- Both methods are making similar cell type assignments
+- Neither method is producing radically different spatial patterns
+ 
+#### 2) Low Match Rates in tumour or preinvasive DCIS cells (Predictions vs Ground Truth)
+DCIS1: 54.9% correct
+
+DCIS2: 60.0% correct
+
+Tumor: 60.9% correct
+
+Stromal: 99.3% correct (not breast cancer, nor hard to identify)
+
+### In a paragraph:
+The moderate match rates between deconvolution predictions and ground truth annotations, combined with the high similarity between Seurat and RCTD predictions, suggest that the spatial transcriptomics deconvolution of the breast TME is fundamentally challenging due to the intermingled nature of cell populations. Both methods produced similar spatial patterns of predicted cell types, indicating that the observed limitations reflect biological complexity rather than methodological inadequacy.
+
+### TME observations:
+- DCIS1 and DCIS2 spots do not overlap - distinct populations
+- myoepithelial cells line the ducts and their spots overlap with both DCIS subtypes - DUCTAL carcinoma in situ (more in Seurat than RCTD image)
+- DCIS2 and tumour regions do some bordering of each other
+- Stromal cells in the breast cancer microenvironment are non-cancerous cells that provide structural and biochemical support to the tumour, affecting its progression, metastasis, and therapy resistance. Stromal cells are concentrated around the tumour which makes sense because of the well-characterised desmoplastic response
+- Endothelial spots are scattered througout but concentrated around tumour because of angiogenesis (clearer with Seurat?)
+- Macrophages are recruited to stromal-DCIS interfaces - immune infiltration. Similar to Tumour-associated macrophages (TAMs) but at site of preinvasive population. Some TAMs seen too (more in RCTD)
+
+### Final evaluation:
+Biologically meaningful spatial patterns that make oncological sense are still captured by the prediction algorithms.
+
+And as always, remember the limitations.
+
+Please create an issue if this workflow could be improved or made more robust.
+
+## References:
+
+Written at end of `TME.R` script, most called using `citation()`
---
+++ TME.R
@@ -0,0 +1,316 @@
+# Setup: Import Packages and Query Dataset ####
+
+# My usual options for seurat analysis
+options(timeout=10000000) # no timeouts
+options(future.globals.maxSize = 1e12) # allow large variables
+
+# pkg list contains some leftovers from OSTA
+pacman::p_load(OSTA.data, # ebook data
+               tidyverse, 
+               Seurat, 
+               patchwork,
+               visiumStitched, # Interoperation
+               CARDspa, # CARD deconvolution for future version
+               pheatmap,
+               SpatialExperiment, # read in data
+               VisiumIO, # for reading in a spatial dataset
+               hdf5r, # for reading in a spatial dataset
+               devtools, # install remote pkgs
+               RNiftyReg, # Quantify img similarity
+               png, # visual comparison
+               pals, # colour scheme
+               caret#, # confusion matrix
+               
+)
+#devtools::install_github("dmcable/spacexr", build_vignettes = FALSE)
+library(spacexr) # RCTD deconvolution for future version
+
+
+# retrieve dataset from OSF repository
+id <- "Visium_HumanBreast_Janesick"
+pa <- OSTA.data_load(id)
+dir.create(td <- tempfile())
+unzip(pa, exdir=td)
+
+# Make Seurat obj out of Query Dataset ####
+
+visium_breast <- 
+  Load10X_Spatial(
+    file.path(td, "outs"),
+    filename = "filtered_feature_bc_matrix.h5",
+    assay = "Spatial",
+    slice = "slice1",
+    bin.size = NULL,
+    filter.matrix = TRUE,
+    to.upper = FALSE,
+    image = NULL,
+    image.name = "tissue_lowres_image.png",
+    segmentation.type = NULL,
+    compact = TRUE
+  )
+
+visium_breast <- UpdateSeuratObject(visium_breast)
+
+# Ground truth (spot-level type annotation) for later ####
+df <- read.csv(file.path(td, "annotation.csv"))
+cs <- match(colnames(visium_breast), df$Barcode)
+visium_breast$anno <- factor(df$Annotation[cs])
+
+# Get QC metrics ####
+
+# I used the QC values in OSTA so I could benchmark the downstream outputs against theirs
+
+# Num RNA molecules per spot
+plot1 <- VlnPlot(visium_breast, features = "nCount_Spatial", pt.size = 0.1) + NoLegend()
+plot2 <- SpatialFeaturePlot(visium_breast, features = "nCount_Spatial", pt.size.factor = 3) + theme(legend.position = "right")
+wrap_plots(plot1, plot2) 
+
+# num unique genes per spot
+plot3 <- VlnPlot(visium_breast, features = "nFeature_Spatial", pt.size = 0.1) + NoLegend()
+wrap_plots(plot3, plot2)
+
+# calculate mitochondrial genes per spot and plot them
+visium_breast[["percent.mt"]] <- PercentageFeatureSet(visium_breast, pattern = "^MT-")   
+plot4 <- VlnPlot(visium_breast, features = "percent.mt", pt.size = 0.1) + NoLegend()
+wrap_plots(plot4, plot2)
+
+# Remove Spots that fail QC ####
+
+dim(visium_breast)
+# 1000 RNA molecules per spot is considered low for this dataset
+# mito counts look ok
+# ignore the warnings. 30 spots should be removed
+visium_breast <- subset(visium_breast, 
+                        subset = nCount_Spatial > 1000 # & percent.mt < 5 
+)
+dim(visium_breast)
+
+# SCTransform, cluster Query dataset ####
+
+visium_breast <- SCTransform(visium_breast, assay = "Spatial")
+visium_breast <- visium_breast %>% RunPCA(assay = "SCT", verbose = FALSE) %>%
+  FindNeighbors(reduction = "pca", dims = 1:30) %>%
+  FindClusters() %>%
+  RunUMAP(reduction = "pca", dims = 1:30)
+
+# Visualise clusters ####
+
+# p1 = visualise clustering in UMAP space, p2 is visualised as image overlay
+p1 <- UMAPPlot(visium_breast, label = TRUE) + ggtitle("UMAP plot")
+p2 <- SpatialDimPlot(visium_breast, label = TRUE, label.size = 5, pt.size.factor = 3) + ggtitle("UMAP clusters over Image")
+p3 <- SpatialDimPlot(visium_breast, label = TRUE, label.size = 5, pt.size.factor = 3, image.alpha = 0) + ggtitle("UMAP clusters - No Image")
+p1 + p2 + p3
+
+# Check clusters against ground truth ####
+
+Idents(visium_breast) <- "anno"
+p1 <- SpatialDimPlot(visium_breast, label = TRUE, label.size = 0, pt.size.factor = 3) + ggtitle("Ground Truth with img")
+p2 <- SpatialDimPlot(visium_breast, label = TRUE, image.alpha = 0, label.size = 4, pt.size.factor = 3) + ggtitle("Ground Truth without img")
+p1+p2+p3
+
+# Import, prepare deconvolution reference ####
+
+# We are provided with single cell reference data from 10X for deconvolution 
+# using the algorithms FindTransferAnchors, RCTD and CARD
+# This is the CHROMIUM set, the experimental data is the VISIUM set
+# retrieve dataset from OSF repo
+id <- "Chromium_HumanBreast_Janesick"
+pa <- OSTA.data_load(id)
+dir.create(td <- tempfile())
+unzip(pa, exdir=td)
+
+chromium_breast <- Read10X_h5(file.path(td, "filtered_feature_bc_matrix.h5"), 
+                              use.names = TRUE, unique.features = TRUE)
+chromium_metadata <- read.csv(file.path(td, "cell_metadata.csv"), row.names = 1)
+
+# Streamline demo by removing mixture spots,
+# consolidating and abbreviating metadata columns
+chromium_ann <- chromium_metadata$Annotation
+chromium_ann[grepl("Hyb", chromium_ann)] <- NA
+metadata_short <- c(
+  "B Cell"="B", "T Cell"="T", "Mac"="macro", "Mast"="mast", 
+  "DCs"="dendritic", "Peri"="perivas", "End"="endo", 
+  "Str"="stromal", "Inv"="tumor", "Myo"="myoepi")
+for (pattern_name in names(metadata_short)) {
+  chromium_ann[grep(pattern_name, chromium_ann)] <- metadata_short[pattern_name]
+}
+chromium_ann <- gsub("\\s", "", chromium_ann)
+chromium_metadata$ShortName <- chromium_ann
+
+# Are all rownames and colnames equal?
+all(rownames(chromium_metadata) == colnames(chromium_breast))
+
+# Reorder metadata to exactly match cell barcodes
+#chromium_metadata <- chromium_metadata[colnames(chromium_breast),]
+
+chromium_breast <- CreateSeuratObject(counts = chromium_breast, meta.data = chromium_metadata)
+chromium_breast <- UpdateSeuratObject(chromium_breast)
+
+Idents(chromium_breast) <- "ShortName"
+
+chromium_breast <- SCTransform(chromium_breast, assay = "RNA")
+chromium_breast <- chromium_breast %>% RunPCA(assay = "SCT", verbose = FALSE) %>%
+  FindNeighbors(reduction = "pca", dims = 1:30) %>%
+  FindClusters() %>%
+  RunUMAP(reduction = "pca", dims = 1:30)
+
+# Deconvolute, ID cells with FindTransferAnchors() ####
+
+anchors <- FindTransferAnchors(reference = chromium_breast, 
+                               query = visium_breast, 
+                               normalization.method = "SCT",
+                               npcs = 50)
+
+predictions.assay <- TransferData(anchorset = anchors, 
+                                  refdata = chromium_breast$ShortName, 
+                                  prediction.assay = TRUE,
+                                  weight.reduction = visium_breast[["pca"]], 
+                                  dims = 1:50)
+
+visium_breast[["predictions"]] <- predictions.assay
+
+DefaultAssay(visium_breast) <- "predictions"
+
+# Visualise Cell type predictions ####
+
+cell.types.of.interest <- c("B", "DCIS1", "DCIS2", "T", "dendritic", "endo", "macro", "mast", "myoepi", "perivas", "stromal", "tumor")
+
+SpatialFeaturePlot(visium_breast, features = cell.types.of.interest, alpha = c(0.1, 1), pt.size.factor = 3)
+
+SpatialFeaturePlot(visium_breast, 
+                   features = cell.types.of.interest, 
+                   image.alpha = 0, 
+                   pt.size.factor = 3.5) & 
+  scale_fill_gradientn(colors = pals::jet(), 
+                       limits = c(0, 1), 
+                       oob = scales::squish) 
+
+# Calculate match rate for TME elements ####
+
+# Extract predicted cell type (highest score)
+visium_breast$predicted_id <- rownames(visium_breast[["predictions"]])[
+  apply(visium_breast[["predictions"]]@data, 2, which.max)
+]
+
+# Define clear, unambiguous mappings only
+clear_mapping <- c(
+  "DCIS #1" = "DCIS1",
+  "DCIS #2" = "DCIS2",
+  "invasive" = "tumor",
+  "stromal" = "stromal"
+)
+
+# Subset to spots with clear ground truth
+visium_clear <- visium_breast[, visium_breast$anno %in% names(clear_mapping)]
+
+# Map ground truth to prediction names
+# (!!! captures the name mapping scheme)
+visium_clear$anno_mapped <- recode(visium_clear$anno, !!!clear_mapping)
+
+# Extract predictions for these spots
+visium_clear$predicted_id <- rownames(visium_clear[["predictions"]])[
+  apply(visium_clear[["predictions"]]@data, 2, which.max)
+]
+
+# Calculate match rate per cell type
+match_rate <- sapply(unique(visium_clear$anno_mapped), function(celltype) {
+  spots <- which(visium_clear$anno_mapped == celltype)
+  correct <- sum(visium_clear$predicted_id[spots] == celltype, na.rm = TRUE)
+  return(correct / length(spots) * 100)
+})
+
+# Print match rates
+for(celltype in names(match_rate)) {
+  total <- sum(visium_clear$anno_mapped == celltype)
+  correct <- sum(visium_clear$predicted_id[visium_clear$anno_mapped == celltype] == celltype)
+  cat(sprintf("%-10s: %5.1f%% (%d/%d)\n", 
+              celltype, match_rate[celltype], correct, total))
+}
+
+# Print success rate and spot total
+overall_accuracy <- sum(visium_clear$predicted_id == visium_clear$anno_mapped) / ncol(visium_clear) * 100
+cat(sprintf("\nOverall Accuracy: %.1f%%\n", overall_accuracy))
+cat(sprintf("Total spots: %d\n", ncol(visium_clear)))
+
+# Confusion matrix of clear, unambiguous mappings
+cm_clear <- table(Predicted = visium_clear$predicted_id, 
+                  Ground_Truth = visium_clear$anno_mapped)
+# Only keep non-empty columns
+cm_clear <- cm_clear[, colSums(cm_clear) > 0, drop = FALSE]
+cm_clear <- cm_clear[rowSums(cm_clear) > 0, , drop = FALSE]
+print(cm_clear)
+
+# Column percentages for heatmap
+col_pct <- prop.table(cm_clear, 2) * 100
+
+pheatmap(col_pct,
+         main = paste("Deconvolution Performance - Clear Cell Types\n(Overall Accuracy:", 
+                      round(overall_accuracy, 1), "%)"),
+         color = colorRampPalette(c("white", "#2E86C1"))(50),
+         display_numbers = TRUE,
+         number_format = "%.1f",
+         cluster_rows = FALSE,
+         cluster_cols = FALSE,
+         angle_col = 45)
+
+# Quantify similarity between Seurat and Bioconductor deconvolutions ####
+
+# Using NMI and NiftyReg
+
+# Read images in.
+# You can replace data/seurat_FindTransferAnchors_img.png with your own image
+seurat_FindTransferAnchors_img <- readPNG("data/seurat_FindTransferAnchors_img.png")
+OSTA_RCTD_img <- readPNG("data/Screenshot_2026-06-10_18-47-50.png")
+
+# NMI before NiftyReg affine registrations
+print(paste("NMI before:", similarity(seurat_FindTransferAnchors_img, OSTA_RCTD_img, interpolation = 3L,
+                                      threads = getOption("RNiftyReg.threads")))) 
+
+# Array with affine registrations (see readme)
+nmi_registered <- niftyreg(seurat_FindTransferAnchors_img, 
+                           OSTA_RCTD_img, scope = "affine",
+                           interpolation = 3L)
+
+# NMI after NiftyReg affine registrations
+score_after <- similarity(nmi_registered$image, OSTA_RCTD_img, 
+                          interpolation = 3L,
+                          threads = getOption("RNiftyReg.threads"))
+print(paste("NMI after:", score_after)) 
+
+# Overlayed image of both sets of plots
+writePNG(nmi_registered$image, "data/aligned_seurat_to_osta.png")
+
+
+
+# References ####
+
+# HL Crowell*°, Y Dong*, I Billato, P Cai, M Emons, S Gunz, B Guo, M Li, A Mahmoud, A Manukyan, H Pagès, P Panwar, S Rao, CJ Sargeant, L Shepherd Kern, M Ramos, J Sun, M Totty, VJ Carey, Y Chen, L Collado-Torres, S Ghazanfar, KD Hansen, K Martinowich, KR Maynard, E Patrick, D Righelli, D Risso, S Tiberi, L Waldron, R Gottardo†°, MD Robinson†°, SC Hicks†°, LM Weber†°. Orchestrating spatial transcriptomics analysis with Bioconductor. bioRxiv (2025). DOI: 10.1101/2025.11.20.688607
+
+# Janesick, A., Shelansky, R., Gottscho, A.D. et al. High resolution mapping of the tumor microenvironment using integrated single-cell, spatial and in situ analysis. Nat Commun 14, 8353 (2023). https://doi.org/10.1038/s41467-023-43458-x
+
+citation("OSTA.data")
+citation("tidyverse") 
+citation("Seurat")
+citation("patchwork")
+citation("visiumStitched")
+citation("CARDspa")
+citation("pheatmap")
+citation("SpatialExperiment")
+citation("VisiumIO")
+citation("hdf5r")
+citation("devtools")
+citation("RNiftyReg")
+citation("png")
+citation("pals")
+citation("spacexr")
+
+# Session Info ####
+
+sink(paste0("session_info_", Sys.Date(), ".txt"))
+sessionInfo()
+sink()
+
+# Use this link to compare your predictions with the predictions the
+# ebook authors got with RCTD and CARD algorithms
+# (their images are flipped vertically)
+# https://bioconductor.org/books/release/OSTA/pages/seq-deconvolution.html#visualization
